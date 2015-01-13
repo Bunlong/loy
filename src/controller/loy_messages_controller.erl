1f4b72e0e76c24df6588eb8ca95347370b1592c0
Bunlong <bunlong.van@gmail.com>
2015-01-13 11:44:57 +0700

    add view

--- src/controller/loy_messages_controller.erl
+++ src/controller/loy_messages_controller.erl
@@ -1,5 +1,24 @@
 -module(loy_messages_controller, [Req]).
 -compile(export_all).
+-export([receive_message/2]).
 
 index('GET', []) ->
-  {json, [{messages, "Hello, world!"}]}.
+  Messages = boss_db:find(message, []),
+  Timestamp = boss_mq:now("Channel"),
+  {ok, [{timestamp, Timestamp}, {messages, Messages}]}.
+
+create('GET', []) ->
+  ok;
+create('POST', []) ->
+  Message = Req:post_param("message"),
+  NewMessage = message:new(id, Message, erlang:now()),
+  case NewMessage:save() of
+    {ok, SavedMessage} ->
+      {redirect, [{action, "create"}]};
+    {error, ErrorList} ->
+      {ok, [{errors, ErrorList}, {new_msg, NewMessage}]}
+  end.
+
+receive_message('GET', [LastTimestamp]) ->
+  {ok, Timestamp, Message} = boss_mq:pull("Channel", list_to_integer(LastTimestamp)),
+  {json, [{timestamp, Timestamp}, {message, Message}]}.
--- src/model/message.erl
+++ src/model/message.erl
@@ -1,2 +1,8 @@
 -module(message, [Id, Message, CreatedAt]).
 -compile([export_all]).
+
+after_create() ->
+  send_message(THIS).
+
+send_message(NewMessage) ->
+  {ok, Timestamp} = boss_mq:push("Channel", NewMessage).
